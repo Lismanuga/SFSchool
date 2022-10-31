2c9fc3e3c4ad95dae872cb873b3ad2c645dbae17
Bohdan <lismanuga@gmail.com>
2022-10-31 16:45:37 +0200

    finished task integration

--- src/lwc/weatherBadge/weatherBadge.js
+++ src/lwc/weatherBadge/weatherBadge.js
@@ -4,20 +4,14 @@ import weatherCallout from '@salesforce/apex/WeatherController.weatherCallout';
 
 export default class weatherBadge extends LightningElement {
 
-    @track test
-    constructor(){
-        super();
+    @track test;
+    connectedCallback(){
         this.tempAndMetric();
     }
 
-    @api
-    async tempAndMetric(){
-        await weatherCallout().then(somedata => this.test = somedata);
-        console.log(weatherCallout().then(somedata => console.log(somedata)));
-    }
 
-    get geter(){
-        return this.tempAndMetric();
+    tempAndMetric(){
+        weatherCallout().then(somedata => this.test = somedata);
     }
 
 }
